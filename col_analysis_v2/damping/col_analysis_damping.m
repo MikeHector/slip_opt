814c7bd2a6df78c496cc195b5075c69687633394
Mike Hector <hectorm@oregonstate.edu>
2018-07-10 19:04:09 -0700

    Added figure to col_analysis_damping; changed analysis file structure

--- col_analysis_v2/damping/col_analysis_damping.m
+++ col_analysis_v2/damping/col_analysis_damping.m
@@ -9,7 +9,7 @@ if record_video==1
     v.FrameRate=10;
     open(v);
 end
-strucc = dir('C:\\Users\mike-\Documents\DRL\collocation\opt_results\damping_results\opt_damping_*');
+strucc = dir('D:\Documents\DRL\slip_opt\opt_results\damping_results\opt_damping_*'); %dir('C:\\Users\mike-\Documents\DRL\collocation\opt_results\damping_results\opt_damping_*');
 
 cmax = 410;
 fig = figure;
@@ -23,7 +23,7 @@ axis([0,cmax, 0, 10]); xlabel('damping'); ylabel('cost');
 subplot(2,2,4); an4 = plot(1,1);
 axis([-.25, .5, -.12, .12]); xlabel('x'); ylabel('xcop')
 % an2 = plot(2,2);
-title = title('wut');
+title1 = title('wut');
 % axis([-0.25, 0.25, .1, .9])
 
 % legend('leg torque', 'ankle torque')
@@ -32,7 +32,7 @@ xlabel('time')
 
 for i = 1:length(strucc)
     filename = strucc(i).name;
-    filename = strcat('C:\\Users\mike-\Documents\DRL\collocation\opt_results\damping_results\', filename);
+    filename = strcat('D:\Documents\DRL\slip_opt\opt_results\damping_results\', filename); %strcat('C:\\Users\mike-\Documents\DRL\collocation\opt_results\damping_results\', filename);
     load(filename)
     results{i} = opt_results;
     c(i) = opt_results.c;
@@ -55,6 +55,10 @@ end
 an32.XData = c_graph;
 an32.YData = cost_graph;
 for i = 1:numel(results)
+    %Make energy shared figure
+    energy_leg(i) = sum(results_sorted_c{i}.Tleg.^2);
+    energy_ankle(i) = sum(results_sorted_c{i}.Tankle.^2);
+    
     if results_sorted_c{i}.flag > 0
         time = results_sorted_c{i}.t;
         leg_response = results_sorted_c{i}.Tleg;
@@ -64,6 +68,10 @@ for i = 1:numel(results)
         r = results_sorted_c{i}.r;
         k = results_sorted_c{i}.k;
         xcop = -ankle_response .* r ./(k .*(results_sorted_c{i}.r0 -r).* y);
+        
+        if max(leg_response) > 49.9
+%             pause
+        end
 
         an1.XData = x;
         an1.YData = y;
@@ -81,8 +89,8 @@ for i = 1:numel(results)
         an4.YData = xcop;
 
         drawnow
-        title.String = ['damping = ', num2str(floor(results_sorted_c{i}.c))];
-        pause(.05)
+        title1.String = ['damping = ', num2str(floor(results_sorted_c{i}.c))];
+%         pause(.05)
         if record_video==1
             F=getframe(gcf);
             writeVideo(v,F);
@@ -94,6 +102,20 @@ if record_video == 1
     close(v)
 end
 
+%Make energy shared figure
+[cUnique, indUnique] = unique(c);
+% barArray = [energy_leg(indUnique)' energy_ankle(indUnique)']; 
+% figure
+% abar = bar(cUnique',barArray, 'stacked');
+figure;
+plot(cUnique, energy_leg(indUnique)); hold on; 
+plot(cUnique, energy_ankle(indUnique),'r');
+a = line([71 71],[0, 12*10^4]); a.LineStyle = '--';
+xlabel('Damping')
+ylabel('Energy')
+legend('Leg energy', 'Ankle energy','Leg begins saturation')
+title('Optimal energies of actuators through stance')
+% a.Color = 'k';
 % figure
 % subplot(2,2,1)
 % 
